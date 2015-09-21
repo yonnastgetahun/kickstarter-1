eb216b6ef25ec85cdb16bfec53c8319fb3110533
yonnastgetahun <yonnastgetahun@gmail.com>
2015-09-20 22:43:40 -0700

    Update share.js

--- share/share.js
+++ share/share.js
@@ -23,7 +23,7 @@ $(function() {
     
     $('.twitter').on('click', function() {
         var loc = encodeURIComponent('http://bit.ly/earthorbit'),
-                title = "Spacedge Love Declarations — ",
+                title = "Space Edge Love Declarations — ",
                 w = 580, h = 300,
                 left = (screen.width/2)-(w/2),
                 top = (screen.height/2)-(h/2);
